cc52eeae90600752fb41cef4fe2c0bdf08a9a561
ケイト <55351037+kateBea@users.noreply.github.com>
2022-07-20 03:42:12 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,11 +4,13 @@
 #include <util/delay.h>
 #include <stdint.h>
 
+#define F_CPU 16000000UL
+
 int main()
 {
     // config I/O ports
-    PORTC   = 0B11111111;
-    DDRC    = 0B00000000;
+    PORTB   = 0B11111111;
+    DDRB    = 0B00000000;
 
 
     uint32_t            counter {  };
@@ -20,12 +22,12 @@ int main()
         ++counter;
 
         // turn on led according to counter value
-        DDRC = static_cast<uint8_t> (counter);
+        DDRB = static_cast<uint8_t> (counter);
         _delay_ms(delay_value);
 
         if (counter == limit) 
         {
-            DDRC    = 0b00000000;
+            DDRB    = 0b00000000;
             counter = 0;
         }
     }
