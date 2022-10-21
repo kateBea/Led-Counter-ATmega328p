5d7ef8e7fa379908d74139db69cd3cd6de5a87b5
ケイト <55351037+kateBea@users.noreply.github.com>
2022-10-21 05:41:08 +0200

    minor changes

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
-// Simple led counter program
-
-#define F_CPU 16000000UL    //define CPU clock cylces in Herz for usage on _delay_ms()
+// CPU clock cylces in Herz for usage on _delay_ms()
+// #define F_CPU 16000000UL
+//
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -9,21 +9,16 @@
 int main()
 {
     // config I/O ports
-    PORTB   = 0B00000000; // clear PORTB pins on startup
-    DDRB    = 0B11111111; // setup PORTB pins as output
-
-
-    uint8_t             counter {  };
+    PORTB = 0B00000000; // clear PORTB pins on startup
+    DDRB = 0B11111111; // setup PORTB pins as output
 
     // delay amount for usage in _delay_ms()
-    constexpr uint16_t  delay_value { 250 };
+    constexpr uint16_t delay_value{ 250 };
+    uint8_t counter {};
 
     while (true)
     {
-        ++counter;
-
-        // turn on leds according to counter value
-        PORTB = counter;
+        PORTB = counter++;
 
         // delay in ms
         _delay_ms(delay_value);
